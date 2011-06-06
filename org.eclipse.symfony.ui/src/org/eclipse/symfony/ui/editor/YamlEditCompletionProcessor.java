b905803917e8af49c2d66d8e347b74d31eacfc88
Robert Gruendler <r.gruendler@gmail.com>
2011-06-06 17:00:51 +0200

    added yml source viewer extension

---
+++ org.eclipse.symfony.ui/src/org/eclipse/symfony/ui/editor/YamlEditCompletionProcessor.java
@@ -0,0 +1,7 @@
+package org.eclipse.symfony.ui.editor;
+
+import org.dadacoalition.yedit.template.YEditCompletionProcessor;
+
+public class YamlEditCompletionProcessor extends YEditCompletionProcessor {
+
+}
