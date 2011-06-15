50e60a859b5060094ee13323a0e8c407577b9ee1
Robert Gruendler <robert@dubture.com>
2011-06-15 20:56:55 +0200

    removed yedit/twig dependency - will be moved to separate plugins

--- org.eclipse.symfony.ui/src/org/eclipse/symfony/ui/editor/YamlEditCompletionProcessor.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.symfony.ui.editor;
-
-import org.dadacoalition.yedit.template.YEditCompletionProcessor;
-
-public class YamlEditCompletionProcessor extends YEditCompletionProcessor {
-
-}
