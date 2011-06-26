48621e6842ec92008ac3cb6c345e2fb4a1d26a9a
Robert Gruendler <robert@dubture.com>
2011-06-26 23:05:50 +0200

    added SymfonyHyperlinkDetector
    Currently enables opening Service implementations when ctrl+clicking services aliases in PHP code
    Future features documented in Detector class.

--- org.eclipse.symfony.core/src/org/eclipse/symfony/core/compiler/SourceElementRequestor.java
+++ org.eclipse.symfony.core/src/org/eclipse/symfony/core/compiler/SourceElementRequestor.java
@@ -1,7 +1,21 @@
 package org.eclipse.symfony.core.compiler;
+
+
+import org.eclipse.dltk.ast.Modifiers;
 import org.eclipse.dltk.ast.declarations.TypeDeclaration;
+import org.eclipse.dltk.ast.expressions.Expression;
+import org.eclipse.dltk.ast.references.VariableReference;
+import org.eclipse.dltk.ast.statements.Statement;
+import org.eclipse.dltk.compiler.ISourceElementRequestor;
 import org.eclipse.php.core.compiler.PHPSourceElementRequestorExtension;
+import org.eclipse.php.internal.core.compiler.ast.nodes.Assignment;
 import org.eclipse.php.internal.core.compiler.ast.nodes.ClassDeclaration;
+import org.eclipse.php.internal.core.compiler.ast.nodes.FullyQualifiedReference;
+import org.eclipse.php.internal.core.compiler.ast.nodes.PHPCallExpression;
+import org.eclipse.php.internal.core.compiler.ast.nodes.Scalar;
+import org.eclipse.symfony.core.SymfonyCoreConstants;
+import org.eclipse.symfony.core.model.Service;
+import org.eclipse.symfony.core.util.ModelUtils;
 
 
 /**
@@ -16,25 +30,127 @@ import org.eclipse.php.internal.core.compiler.ast.nodes.ClassDeclaration;
 public class SourceElementRequestor extends PHPSourceElementRequestorExtension {
 
 	private ClassDeclaration currentClass;
+	private boolean inController;
+	
 	
 	
 	@Override
 	public boolean visit(TypeDeclaration s) throws Exception {
 		
-
 		if (s instanceof ClassDeclaration) {			
-			currentClass = (ClassDeclaration) s;			
-		}
+			currentClass = (ClassDeclaration) s;
+			for (Object o : currentClass.getSuperClasses().getChilds()) {
+
+				if (o instanceof FullyQualifiedReference) {
+
+					FullyQualifiedReference superReference = (FullyQualifiedReference) o;					
+
+					//TODO: find a way to check against the FQCN
+					// via the UseStatement
+					if (/*superReference.getNamespace().equals(SymfonyCoreConstants.CONTROLLER_NS) 
+							&& */superReference.getName().equals(SymfonyCoreConstants.CONTROLLER_CLASS)) {
+						
+						// the ControllerIndexer does the actual work of parsing the
+						// the relevant elements inside the controller
+						// which are then being collected in the endVisit() method
+						inController = true;
+						
+					}
+				}
+			}
+		}		
 	
 		return true;
 	}
+	
+	@Override
+	public boolean visit(Statement st) throws Exception {
+
+		return true;
+	}
+	
+	
+	@Override
+	public boolean visit(Expression st) throws Exception {
+
+		if (st instanceof Assignment) {
+
+			Assignment s = (Assignment) st;
+
+			Service service = null;
+			
+			if (s.getVariable().getClass() == VariableReference.class) {
+
+				VariableReference var = (VariableReference) s.getVariable();		
+
+				// A call expression like $foo = $this->get('bar');
+				//
+				if (s.getValue().getClass() == PHPCallExpression.class) {
+
+					PHPCallExpression exp = (PHPCallExpression) s.getValue();
+
+					// are we requesting a Service?
+					if (exp.getName().equals("get")) {
+						
+						service = ModelUtils.extractServiceFromCall(exp);
+						
+						if (service != null) {
+
+							try {
+
+								ISourceElementRequestor.TypeInfo importInfo =
+								        new ISourceElementRequestor.TypeInfo();
+								
+								Scalar scalar = (Scalar) exp.getArgs().getChilds().get(0);
+
+								System.err.println(scalar.sourceStart() + " " + scalar.sourceEnd());
+								importInfo.name = service.getId();
+								importInfo.modifiers = Modifiers.AccPublic;
+								importInfo.nameSourceStart = scalar.sourceStart();
+								importInfo.nameSourceEnd = scalar.sourceEnd();
+								
+								fRequestor.enterType(importInfo);
+								fRequestor.exitType(scalar.sourceEnd());
+								
+								System.err.println("in service " + importInfo.nameSourceStart + " " + importInfo.nameSourceEnd);	
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+
+					// a more complex expression like
+					// $form = $this->get('form.factory')->create(new ContactType());
+					} else if (exp.getReceiver().getClass() == PHPCallExpression.class) {
+
+						// try to extract a service if it's a Servicecontainer call
+						service = ModelUtils.extractServiceFromCall((PHPCallExpression) exp.getReceiver());
+						
+						// nothing found, return
+						if (service == null || exp.getCallName() == null) {
+							
+							System.err.println("nothing found");
+							return true;
+						}
+					}
+				} 
+			}
+		}		
+		
+	
+		return true;
+	}
+	
 	
 	@Override
 	public boolean endvisit(TypeDeclaration s) throws Exception {
 				
 		if (currentClass != null) {
 						
+
 		}
+		
+		inController = false;
 		
 		currentClass = null;
 		return true;
---
+++ org.eclipse.symfony.ui/src/org/eclipse/symfony/ui/editor/hyperlink/SymfonyHyperlinkDetector.java
@@ -0,0 +1,183 @@
+package org.eclipse.symfony.ui.editor.hyperlink;
+
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.core.index2.search.ISearchEngine.MatchRule;
+import org.eclipse.dltk.core.search.IDLTKSearchScope;
+import org.eclipse.dltk.core.search.SearchEngine;
+import org.eclipse.dltk.internal.ui.editor.EditorUtility;
+import org.eclipse.dltk.internal.ui.editor.ModelElementHyperlink;
+import org.eclipse.dltk.ui.actions.OpenAction;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.php.internal.core.model.PhpModelAccess;
+import org.eclipse.php.internal.ui.editor.PHPStructuredEditor;
+import org.eclipse.php.internal.ui.editor.hyperlink.PHPHyperlinkDetector;
+import org.eclipse.symfony.core.SymfonyCorePlugin;
+import org.eclipse.symfony.core.model.ModelManager;
+import org.eclipse.symfony.core.model.Service;
+
+
+/**
+ * {@link SymfonyHyperlinkDetector} currently detects
+ * Services in PHP code. Ctrl+Click will open the 
+ * Implementation.
+ * 
+ * Future Hyperlink-Detectors will provide:
+ * 
+ * 1. Open templates when clicking on @Template() annotations or render('...') functions
+ * 2. Open Controllers when clicking on ViewVariables
+ * 3. Open Definitions of routes when clicking on routes 
+ * 
+ * 
+ * @author "Robert Gruendler <r.gruendler@gmail.com>"
+ *
+ */
+@SuppressWarnings("restriction")
+public class SymfonyHyperlinkDetector extends PHPHyperlinkDetector {
+
+	public SymfonyHyperlinkDetector() {
+
+	}
+
+
+	@Override
+	public IHyperlink[] detectHyperlinks(ITextViewer textViewer,
+			IRegion region, boolean canShowMultipleHyperlinks) {
+
+		
+		final PHPStructuredEditor editor = org.eclipse.php.internal.ui.util.EditorUtility
+				.getPHPEditor(textViewer);
+		if (editor == null) {
+			return null;
+		}
+
+		if (region == null) {
+			return null;
+		}
+
+		IModelElement input = EditorUtility.getEditorInputModelElement(editor,
+				false);
+		if (input == null) {
+			return null;
+		}
+		
+		IDocument document = textViewer.getDocument();
+		int offset = region.getOffset();
+		
+		try {
+			IRegion wordRegion = findWord(document, offset);
+			
+			if (wordRegion == null)
+				return null;
+
+			String service = document.get(wordRegion.getOffset(), wordRegion.getLength());			
+			Service s = ModelManager.getInstance().findService(service, input.getScriptProject().getPath());
+			
+			if (s == null) {
+				return null;
+			}
+			
+			IDLTKSearchScope scope = SearchEngine.createSearchScope(input.getScriptProject());
+			
+			IType[] types = PhpModelAccess.getDefault().findTypes(s.getNamespace(), s.getClassName(), MatchRule.EXACT, 0, 0, scope, null);
+			
+			if (types.length != 1) {
+				SymfonyCorePlugin.debug("No service found");
+				return null;
+			}
+			
+			IType type = types[0];			
+			final IHyperlink link;
+			
+			link = new ModelElementHyperlink(wordRegion, type,
+					new OpenAction(editor));			
+			
+			return new IHyperlink[] { link };
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	
+	public static IRegion findWord(IDocument document, int offset) {
+
+		boolean namespacesSupported = true;
+		
+		int start = -2;
+		int end = -1;
+
+		try {
+			int pos = offset;
+			char c;
+			
+
+			int rightmostNsSeparator = -1;
+			while (pos >= 0) {
+				c = document.getChar(pos);
+				
+				if (!Character.isJavaIdentifierPart(c)
+						&& (!namespacesSupported || c != '\'')) {
+					break;
+				}
+				if (namespacesSupported && c == '\''
+						&& rightmostNsSeparator == -1) {
+					rightmostNsSeparator = pos;
+				}
+				--pos;
+			}
+			start = pos;
+
+			pos = offset;
+			int length = document.getLength();
+
+			while (pos < length) {
+				c = document.getChar(pos);
+				if (!Character.isJavaIdentifierPart(c)
+						&& (!namespacesSupported || c != '\'')) {
+					break;
+				}
+				if (namespacesSupported && c == '\'') {
+					rightmostNsSeparator = pos;
+				}
+				++pos;
+			}
+			end = pos;
+
+			if (rightmostNsSeparator != -1) {
+				if (rightmostNsSeparator > offset) {
+					end = rightmostNsSeparator;
+				} else {
+					start = rightmostNsSeparator;
+				}
+			}
+
+		} catch (BadLocationException x) {
+		}
+
+		if (start >= -1 && end > -1) {
+			if (start == offset && end == offset) {
+				return new Region(offset, 0);
+			} else if (start == offset) {
+				
+				return new Region(start, end - start);
+			} else {
+				
+				return new Region(start + 2, end - start - 2);
+			}
+		}
+
+		
+		return null;
+	}
+
+}
+
