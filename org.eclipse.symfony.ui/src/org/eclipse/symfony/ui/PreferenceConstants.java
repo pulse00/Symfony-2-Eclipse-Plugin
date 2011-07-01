e3d9ba3b90b7eaf26b253b6d9c456ee7064cab15
Robert Gruendler <robert@dubture.com>
2011-07-01 09:45:33 +0200

    removed debug output

--- org.eclipse.symfony.ui/src/org/eclipse/symfony/ui/PreferenceConstants.java
+++ org.eclipse.symfony.ui/src/org/eclipse/symfony/ui/PreferenceConstants.java
@@ -1,12 +1,26 @@
 package org.eclipse.symfony.ui;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.symfony.core.SymfonyCoreConstants;
+import org.eclipse.symfony.core.util.JsonUtils;
 
-public class CorePreferenceConstants {
+public class PreferenceConstants {
 	
 	public interface Keys {
 		public static final String SYNTHETIC_SERVICES = SymfonyCoreConstants.SYNTHETIC_SERVICES;
 	}
 	
+	
 
+	public static IPreferenceStore getPreferenceStore() {
+		return SymfonyUiPlugin.getDefault().getPreferenceStore();
+	}
+	
+	public static void initializeDefaultValues() {
+
+		IPreferenceStore store = getPreferenceStore();
+		String defaultServices = JsonUtils.createDefaultSyntheticServices();
+		store.setDefault(SymfonyCoreConstants.SYNTHETIC_SERVICES, defaultServices);
+		
+	}
 }
