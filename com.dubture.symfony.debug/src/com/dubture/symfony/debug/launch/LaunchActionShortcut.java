2764c33bdaf62fbad763ab24811d4ba3553a0e5a
Robert Gruendler <robert@dubture.com>
2011-10-30 11:31:08 +0100

    moved launch shortcut to proper plugin

---
+++ com.dubture.symfony.debug/src/com/dubture/symfony/debug/launch/LaunchActionShortcut.java
@@ -0,0 +1,319 @@
+package com.dubture.symfony.debug.launch;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.dltk.core.IMethod;
+import org.eclipse.dltk.core.IModelElement;
+import org.eclipse.dltk.core.IScriptProject;
+import org.eclipse.dltk.core.ISourceModule;
+import org.eclipse.dltk.core.IType;
+import org.eclipse.dltk.internal.core.SourceMethod;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.php.debug.core.debugger.parameters.IDebugParametersKeys;
+import org.eclipse.php.internal.core.documentModel.provisional.contenttype.ContentTypeIdForPHP;
+import org.eclipse.php.internal.core.util.FileUtils;
+import org.eclipse.php.internal.debug.core.IPHPDebugConstants;
+import org.eclipse.php.internal.debug.core.PHPDebugPlugin;
+import org.eclipse.php.internal.debug.core.debugger.AbstractDebuggerConfiguration;
+import org.eclipse.php.internal.debug.core.preferences.PHPDebugCorePreferenceNames;
+import org.eclipse.php.internal.debug.core.preferences.PHPDebuggersRegistry;
+import org.eclipse.php.internal.debug.core.preferences.PHPProjectPreferences;
+import org.eclipse.php.internal.debug.ui.PHPDebugUIPlugin;
+import org.eclipse.php.internal.server.core.Server;
+import org.eclipse.php.internal.server.core.manager.ServersManager;
+import org.eclipse.php.internal.server.ui.launching.Messages;
+import org.eclipse.php.internal.server.ui.launching.PHPWebPageLaunchDialog;
+import org.eclipse.php.internal.server.ui.launching.PHPWebPageLaunchShortcut;
+import org.eclipse.php.internal.server.ui.launching.PHPWebPageURLLaunchDialog;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+
+import com.dubture.symfony.debug.util.ServerUtils;
+import com.dubture.symfony.index.dao.Route;
+import com.dubture.symfony.ui.editor.EditorUtility;
+
+
+
+@SuppressWarnings("restriction")
+public class LaunchActionShortcut extends PHPWebPageLaunchShortcut {
+
+	public void launch(ISelection selection, String mode) {
+		if (selection instanceof IStructuredSelection) {
+			
+			ILaunchManager lm = DebugPlugin.getDefault().getLaunchManager();
+			ILaunchConfigurationType  configType = lm.getLaunchConfigurationType(IPHPDebugConstants.PHPServerLaunchType);			
+			searchAndLaunch(((IStructuredSelection) selection).toArray(), mode,
+					configType);
+		}
+	}
+
+	public void launch(IEditorPart editor, String mode) {
+		IEditorInput input = editor.getEditorInput();
+		IFile file = (IFile) input.getAdapter(IFile.class);
+		if (file != null) {
+			
+			ILaunchManager lm = DebugPlugin.getDefault().getLaunchManager();
+			ILaunchConfigurationType  configType = lm.getLaunchConfigurationType(IPHPDebugConstants.PHPServerLaunchType);			
+			
+			searchAndLaunch(new Object[] { file }, mode,
+					configType);
+		}
+	}
+	
+	static ILaunchConfiguration findLaunchConfiguration(IProject project,
+			String fileName, String selectedURL, Server server, String mode,
+			ILaunchConfigurationType configType, boolean breakAtFirstLine,
+			boolean showDebugDialog, IResource res) {
+		ILaunchConfiguration config = null;
+
+		try {
+			ILaunchConfiguration[] configs = DebugPlugin.getDefault()
+					.getLaunchManager().getLaunchConfigurations(configType);
+
+			int numConfigs = configs == null ? 0 : configs.length;
+			for (int i = 0; i < numConfigs; i++) {
+				String configuredServerName = configs[i].getAttribute(
+						Server.NAME, (String) null);
+				String configuredFileName = configs[i].getAttribute(
+						Server.FILE_NAME, (String) null);
+
+				if (configuredFileName != null)
+					if (configuredFileName.equals(fileName)
+							&& server.getName().equals(configuredServerName)) {
+						config = configs[i].getWorkingCopy();
+						break;
+					}
+			}
+
+			if (config == null) {
+				config = createConfiguration(project, fileName, selectedURL,
+						server, configType, mode, breakAtFirstLine,
+						showDebugDialog, res);
+			}
+		} catch (CoreException ce) {
+			ce.printStackTrace();
+		}
+		return config;
+	}
+	
+	
+	
+	public static void searchAndLaunch(Object[] search, String mode,
+			ILaunchConfigurationType configType) {
+		int entries = search == null ? 0 : search.length;
+		for (int i = 0; i < entries; i++) {
+			try {
+				String phpPathString = null;
+				IProject project = null;
+				Object obj = search[i];
+				IResource res = null;
+				if (obj instanceof IModelElement) {
+					IModelElement elem = (IModelElement) obj;
+
+					if (elem instanceof ISourceModule) {
+						res = ((ISourceModule) elem).getCorrespondingResource();
+					} else if (elem instanceof IType) {
+						res = ((IType) elem).getUnderlyingResource();
+					} else if (elem instanceof IMethod) {
+						res = ((IMethod) elem).getUnderlyingResource();
+					}
+					if (res instanceof IFile) {
+						obj = (IFile) res;
+					}
+				}
+
+				if (obj instanceof IFile) {
+					IFile file = (IFile) obj;
+					res = file;
+					project = file.getProject();
+					IContentType contentType = Platform.getContentTypeManager()
+							.getContentType(
+									ContentTypeIdForPHP.ContentTypeID_PHP);
+					if (contentType.isAssociatedWith(file.getName())) {
+						phpPathString = file.getFullPath().toString();
+					}
+				}
+
+				Server defaultServer = ServersManager.getDefaultServer(project);
+				if (defaultServer == null) {
+					PHPDebugPlugin.createDefaultPHPServer();
+					defaultServer = ServersManager.getDefaultServer(project);
+					if (defaultServer == null) {
+						// Sould not happen
+						throw new CoreException(new Status(IStatus.ERROR,
+								PHPDebugUIPlugin.ID, IStatus.OK,
+								Messages.PHPWebPageLaunchShortcut_0, null));
+					}
+				}
+
+				String basePath = PHPProjectPreferences
+						.getDefaultBasePath(project);
+
+				boolean breakAtFirstLine = PHPProjectPreferences
+						.getStopAtFirstLine(project);
+				String selectedURL = null;
+				boolean showDebugDialog = true;
+				if (obj instanceof IScriptProject) {
+					final PHPWebPageLaunchDialog dialog = new PHPWebPageLaunchDialog(
+							mode, (IScriptProject) obj, basePath);
+					final int open = dialog.open();
+					if (open == PHPWebPageLaunchDialog.OK) {
+						defaultServer = dialog.getServer();
+						selectedURL = dialog.getPhpPathString();
+						phpPathString = dialog.getFilename();
+						breakAtFirstLine = dialog.isBreakAtFirstLine();
+						showDebugDialog = false;
+					} else {
+						continue;
+					}
+				}
+
+				if (phpPathString == null) {
+					// Could not find target to launch
+					throw new CoreException(new Status(IStatus.ERROR,
+							PHPDebugUIPlugin.ID, IStatus.OK,
+							Messages.launch_failure_no_target, null));
+				}
+
+				// Launch the app
+				ILaunchConfiguration config = findLaunchConfiguration(project,
+						phpPathString, selectedURL, defaultServer, mode,
+						configType, breakAtFirstLine, showDebugDialog, res);
+				if (config != null) {
+					DebugUITools.launch(config, mode);
+				} else {
+					// Could not find launch configuration or the user
+					// cancelled.
+					// throw new CoreException(new Status(IStatus.ERROR,
+					// PHPDebugUIPlugin.ID, IStatus.OK,
+					// PHPDebugUIMessages.launch_failure_no_config, null));
+				}
+			} catch (CoreException ce) {
+				final IStatus stat = ce.getStatus();
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						ErrorDialog.openError(
+								PHPDebugUIPlugin.getActiveWorkbenchShell(),
+								Messages.launch_failure_msg_title,
+								Messages.launch_failure_server_msg_text, stat);
+					}
+				});
+			}
+		}
+	}	
+	
+	
+	static ILaunchConfiguration createConfiguration(IProject project,
+			String fileName, String selectedURL, Server server,
+			ILaunchConfigurationType configType, String mode,
+			boolean breakAtFirstLine, boolean showDebugDialog, IResource res)
+			throws CoreException {
+		
+		ILaunchConfiguration config = null;
+		if (!FileUtils.resourceExists(fileName)) {
+			return null;
+		}
+
+		
+		EditorUtility utility = new EditorUtility();
+		Route route = utility.getRouteAtCursor();
+		
+		ILaunchConfigurationWorkingCopy wc = configType.newInstance(null,
+				getNewConfigurationName(utility.getMethod(), route));
+
+		// Set the debugger ID and the configuration delegate for this launch
+		// configuration
+		String debuggerID = PHPProjectPreferences.getDefaultDebuggerID(project);
+		wc.setAttribute(PHPDebugCorePreferenceNames.PHP_DEBUGGER_ID, debuggerID);
+		AbstractDebuggerConfiguration debuggerConfiguration = PHPDebuggersRegistry
+				.getDebuggerConfiguration(debuggerID);
+		wc.setAttribute(
+				PHPDebugCorePreferenceNames.CONFIGURATION_DELEGATE_CLASS,
+				debuggerConfiguration.getWebLaunchDelegateClass());
+
+		wc.setAttribute(Server.NAME, server.getName());
+		
+		wc.setAttribute(Server.FILE_NAME, fileName);
+		wc.setAttribute(Server.BASE_URL, "");
+		wc.setAttribute(IPHPDebugConstants.RUN_WITH_DEBUG_INFO,
+				PHPDebugPlugin.getDebugInfoOption());
+		wc.setAttribute(IPHPDebugConstants.OPEN_IN_BROWSER,
+				PHPDebugPlugin.getOpenInBrowserOption());
+		wc.setAttribute(IDebugParametersKeys.FIRST_LINE_BREAKPOINT,
+				breakAtFirstLine);
+		if (res != null) {
+			wc.setMappedResources(new IResource[] { res });
+		}
+		
+		config = wc.doSave();		
+		String URL = null;
+		
+		if (selectedURL != null) {
+			URL = selectedURL;
+		} else {
+			try {								
+				URL = ServerUtils.constructURL(config, utility.getProject(), route);				
+				if (URL == null)
+					URL = "";
+			} catch (Exception e) {
+				// safe as resolved URL is server.getBaseURL()
+				e.printStackTrace();
+				if (URL == null)
+					URL = "";
+			}
+		}
+		
+		// Display a dialog for selecting the URL.
+		if (showDebugDialog && URL.length() == 0) {
+			String title = (ILaunchManager.DEBUG_MODE.equals(mode) ? Messages.PHPWebPageLaunchShortcut_1
+					: (ILaunchManager.PROFILE_MODE.equals(mode) ? Messages.PHPWebPageLaunchShortcut_2
+							: Messages.PHPWebPageLaunchShortcut_3));
+			PHPWebPageURLLaunchDialog launchDialog = new PHPWebPageURLLaunchDialog(
+					wc, server, title);
+			launchDialog.setBlockOnOpen(true);
+			if (launchDialog.open() != PHPWebPageURLLaunchDialog.OK) {
+				return null;
+			}
+		}
+		config = wc.doSave();
+		return config;
+	}
+	
+	
+	protected static String getNewConfigurationName(SourceMethod sourceMethod, Route route) {
+		
+		String configurationName = Messages.PHPWebPageLaunchShortcut_4;
+		
+		try {
+			
+			if (route.getName() != null) {
+				configurationName = route.getName();
+			} else {
+				configurationName = sourceMethod.getParent().getElementName() + 
+						"::" + sourceMethod.getElementName() + "";				
+			}
+
+		} catch (Exception e) {
+
+		}
+		return DebugPlugin.getDefault().getLaunchManager()
+				.generateLaunchConfigurationName(configurationName);
+	}
+	
+	
+}
