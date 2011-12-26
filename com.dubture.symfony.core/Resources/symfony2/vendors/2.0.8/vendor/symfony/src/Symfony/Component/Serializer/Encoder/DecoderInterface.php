4d11f60aa6a531dcf23337a91a1437551632d52a
Robert Gruendler <robert@dubture.com>
2011-12-26 23:25:00 +0100

    updated to 2.0.8

---
+++ com.dubture.symfony.core/Resources/symfony2/vendors/2.0.8/vendor/symfony/src/Symfony/Component/Serializer/Encoder/DecoderInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Symfony\Component\Serializer\Encoder;
+
+
+/*
+ * This file is part of the Symfony framework.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+/**
+ * Defines the interface of encoders that are able to decode their own format
+ *
+ * @author Jordi Boggiano <j.boggiano@seld.be>
+ */
+interface DecoderInterface
+{
+    /**
+     * Decodes a string into PHP data
+     *
+     * @param string $data   Data to decode
+     * @param string $format Format to decode from
+     *
+     * @return mixed
+     */
+    function decode($data, $format);
+}
--- com.dubture.symfony.ui/src/com/dubture/symfony/ui/preferences/LibraryPreferencePage.java
+++ com.dubture.symfony.ui/src/com/dubture/symfony/ui/preferences/LibraryPreferencePage.java
@@ -59,7 +59,7 @@ implements IWorkbenchPreferencePage {
 
 		options = new String[][] 
 				{ 
-				{ SymfonyVersion.Symfony2_0_7.getAlias(), SymfonyVersion.Symfony2_0_7.getAlias() } 				
+				{ SymfonyVersion.Symfony2_0_8.getAlias(), SymfonyVersion.Symfony2_0_8.getAlias() } 				
 				};			
 
 
