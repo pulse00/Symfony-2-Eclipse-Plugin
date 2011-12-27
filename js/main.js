96bc2773214c0c8b597add8790fd8aea3c96bc87
Robert Gruendler <robert@dubture.com>
2011-12-27 21:49:40 +0100

    added site again

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function initLightbox() {
+	$('a.lightbox').lightBox();
+	$('div.multiShot a').lightBox({fixedNavigation:true});
+}
+
+$(document).ready(function() {	
+	$("#tabs").tabs();
+	$('#featurestab').tabs().addClass('ui-tabs-vertical ui-helper-clearfix');
+	$('#featurestab li').removeClass('ui-corner-top').addClass('ui-corner-left');			
+	$('div.multiShotContainer').tabs();			
+	initLightbox();
+});
