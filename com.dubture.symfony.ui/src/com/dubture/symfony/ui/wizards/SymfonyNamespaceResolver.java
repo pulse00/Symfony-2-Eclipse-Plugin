5b3c45e023b493899cb7815148084d5afc33dfbe
Robert Gruendler <robert@dubture.com>
2012-03-25 09:56:02 +0200

    refs #99

--- com.dubture.symfony.ui/src/com/dubture/symfony/ui/wizards/SymfonyNamespaceResolver.java
+++ com.dubture.symfony.ui/src/com/dubture/symfony/ui/wizards/SymfonyNamespaceResolver.java
@@ -11,17 +11,22 @@ package com.dubture.symfony.ui.wizards;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.dltk.core.IScriptFolder;
 
+import com.dubture.composer.core.model.ModelAccess;
 import com.dubture.pdt.ui.extension.INamespaceResolver;
-import com.dubture.symfony.core.model.SymfonyModelAccess;
 
 public class SymfonyNamespaceResolver implements INamespaceResolver {
 
 	@Override
 	public String resolve(IScriptFolder container) {
 
-		IPath path = container.getPath();
-		return SymfonyModelAccess.getDefault().findNameSpace(container.getScriptProject(), path);
+		IPath path = ModelAccess.getInstance()
+	            .resolve(container.getResource());
+		
+		if (path != null) {
+		    return path.toString().replace("/", "\\");
+		}
+		
+		return null;
 		
 	}
-
 }
