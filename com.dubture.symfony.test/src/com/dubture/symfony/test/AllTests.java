5dd0b06a870f8e0f0e1979d011940961731a3526
Dawid Pakuła <zulus@w3des.net>
2015-02-26 21:31:05 +0100

    Remove doctrine plugin annotation tests Update DLTK test repository

--- com.dubture.symfony.test/src/com/dubture/symfony/test/AllTests.java
+++ com.dubture.symfony.test/src/com/dubture/symfony/test/AllTests.java
@@ -4,14 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.dubture.symfony.test.codeassist.AnnotationCodeAssistTest;
 import com.dubture.symfony.test.codeassist.DoctrineCodeAssistTest;
 import com.dubture.symfony.test.codeassist.ServiceCodeAssistTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AnnotationCommentParserTest.class, AnnotationParserTest.class, IndexTest.class, ModelUtilsTest.class,
 		PathUtilsTest.class, RoutingParserTest.class, TextSequenceUtilityTest.class, XMLParserTest.class,
-		YamlTest.class, ServiceCodeAssistTest.class, DoctrineCodeAssistTest.class, AnnotationCodeAssistTest.class })
+		YamlTest.class, ServiceCodeAssistTest.class, DoctrineCodeAssistTest.class })
 public class AllTests {
 
 }
